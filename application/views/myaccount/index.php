5d561a6c962115d70c32009865e424f47c66f8b4
Yogi Nugraha <yoginugraha43@gmail.com>
2019-12-12 10:47:06 +0700

    membuat edit account, dan add & edit address

--- application/views/myaccount/index.php
+++ application/views/myaccount/index.php
@@ -3,7 +3,7 @@
     <a href="#" class="list-group-item list-group-item-action active">
       Account Information
     </a>
-    <a href="#" class="list-group-item list-group-item-action">Edit Name and E-mail</a>
-    <a href="#" class="list-group-item list-group-item-action">Add or Edit Address</a>
+    <a href="" class="list-group-item list-group-item-action">Edit Name and E-mail</a>
+    <a href="" class="list-group-item list-group-item-action">Add or Edit Address</a>
   </div>
 </div>
