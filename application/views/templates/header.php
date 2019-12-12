43ca23413f3cef82321604e9f45f92379b9b6616
Yogi Nugraha <yoginugraha43@gmail.com>
2019-12-12 09:36:28 +0700

    memindahkan popup modal ke header sehingga bisa di akses di semua halaman

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -48,3 +48,47 @@
             </div>
         </div>
     </nav>
+
+    <!-- modal pup up -->
+
+
+<div class="modal fade" id="modalLogin" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header text-center">
+        <h5 class="modal-title  w-100 font-weight-bold" id="exampleModalLabel">Form Login</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form class="user">
+          <div class="form-group">
+            <input type="text" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
+          </div>
+          <div class="form-group">
+            <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+          </div>
+          <div class="form-group">
+            <div class="custom-control custom-checkbox small">
+              <input type="checkbox" class="custom-control-input" id="customCheck">
+              <label class="custom-control-label" for="customCheck">Remember Me</label>
+            </div>
+          </div>
+          <a href="" class="btn btn-primary btn-user btn-block">
+            Login
+          </a>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <div class="text-center">
+          <a class="small" href="forgot-password.html">Forgot Password?</a>
+        </div>
+        <div class="text-center">
+          <a href="#modalRegister" class="small" data-toggle="modal">Create an Account!</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- end modal pop up -->
