93c273631696d137796cc1ded17e4f9d5ef06326
rioerika <rio_173040041@mail.unpas.ac.id>
2019-12-13 08:12:02 +0700

    memindahkan modal register ke file templates

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -46,48 +46,7 @@
 
 
 
-<!-- modal pop up register -->
-<div class="modal fade" id="modalRegister" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header text-center">
-        <h5 class="modal-title  w-100 font-weight-bold" id="exampleModalLabel">Form Register</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <form class="user">
-          <div class="form-group row">
-            <div class="col-sm-6 mb-3 mb-sm-0">
-              <input type="text" class="form-control form-control-user" id="exampleFirstName" placeholder="First Name">
-            </div>
-            <div class="col-sm-6">
-              <input type="text" class="form-control form-control-user" id="exampleLastName" placeholder="Last Name">
-            </div>
-          </div>
-          <div class="form-group">
-            <input type="email" class="form-control form-control-user" id="exampleInputEmail" placeholder="Email Address">
-          </div>
-          <div class="form-group row">
-            <div class="col-sm-6 mb-3 mb-sm-0">
-              <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
-            </div>
-            <div class="col-sm-6">
-              <input type="password" class="form-control form-control-user" id="exampleRepeatPassword" placeholder="Repeat Password">
-            </div>
-          </div>
-        </form>
-      </div>
-      <div class="modal-footer">
-        <a href="login.html" class="btn btn-primary btn-user btn-block">
-          Register Account
-        </a>
-      </div>
-    </div>
-  </div>
-</div>
-<!--  end modal pop up register -->
+
 
 <!-- Marketing Best Saller -->
 <div class="container padto text-center">
