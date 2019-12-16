1d542058fef6f6d0ba5c43d99ed14ad3b0870799
Yogi Nugraha <yoginugraha43@gmail.com>
2019-12-16 09:49:51 +0700

    mengganti item pada halaman shop dengan item yang sudah diinputkan pada database

--- application/views/shop/index.php
+++ application/views/shop/index.php
@@ -1,23 +1,35 @@
 <div>
-      <div>
+    <div>
         <div>
-          <img src="assets/images/background1.jpeg" class="d-block w-100" alt="...">
-      </div>
+            <img src="assets/images/background1.jpeg" class="d-block w-100" alt="...">
+        </div>
     </div>
 </div>
 
 <div class="container">
-  <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-</a>
-<a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-</a>
+    <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
 </div>
 <div class="row">
-    <a href="" class="col-md-3 mb-4">
+    <?php foreach ($barang as $brg) : ?>
+        <a href="" class="col-md-3 mb-4">
+            <div class="card h-100 shadow-sm">
+                <img src="assets/images/<?= $brg['gambar']; ?>" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title"><?= $brg['nama_barang']; ?></h5>
+                    <p class="card-text"><?= $brg['harga_barang']; ?></p>
+                </div>
+            </div>
+        </a>
+    <?php endforeach; ?>
+
+    <!-- <a href="" class="col-md-3 mb-4">
         <div class="card h-100 shadow-sm">
             <img src="assets/images/Karambit-Gamma-Doppler.jpg" class="card-img-top" alt="...">
             <div class="card-body">
@@ -115,17 +127,7 @@
                 <p class="card-text">IDR 460.000</p>
             </div>
         </div>
-    </a>
-
-    <a href="" class="col-md-3 mb-4">
-        <div class="card h-100 shadow-sm">
-            <img src="assets/images/Karambit-Gamma-Doppler.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Karambit Gamma Doppler</h5>
-                <p class="card-text">IDR 460.000</p>
-            </div>
-        </div>
-    </a>
+    </a> -->
 
 </div>
 </div>
