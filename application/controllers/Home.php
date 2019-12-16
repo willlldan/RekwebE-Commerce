1d2a47f9b97d35c436be7166634ddc438aab0f8d
Yogi Nugraha <yoginugraha43@gmail.com>
2019-12-16 08:07:23 +0700

    setting database

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,8 +1,15 @@
 <?php
 class Home extends CI_Controller
 {
-    public function index()
+    public function __construct()
     {
+        parent::__construct();
+        $this->load->database();
+    }
+
+    public function index($nama = '')
+    {
+
         $data['judul'] = 'Home';
         $data['nama'] = $nama;
         $this->load->view('templates/header', $data);
